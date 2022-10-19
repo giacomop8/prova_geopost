3679b4f318ad7f6a752ca2be8e159b5e8024fc71
giacomop8 <giacomopv@outlook.com>
2022-10-19 17:02:58 -0300

    Atualizando

--- script.js
+++ script.js
@@ -9,48 +9,27 @@ async function getContent() {
         const data = await response.json()
         times = data.Result
 
-        return times
+        sorteando(times)
     } catch (error) {
         console.error(error)        
     }
 }
 
-times = getContent()
-
-grupoFinal = []
-timeSorteado = Math.floor(Math.random() * 32)
-
-for(let i=0; i<32; i++) {
+async function sorteando(times) {
     
-    while(grupoFinal.indexOf(timeSorteado) != -1) {
-        timeSorteado = Math.floor(Math.random() * 32)
+    grupoFinal = []
+    timeSorteado = Math.floor(Math.random() * 32)
+    
+    
+    /* for(let i=0; i<32; i++) {
+        
+        while(grupoFinal.indexOf(timeSorteado) != -1) {
+            timeSorteado = Math.floor(Math.random() * 32)
+        }
+        grupoFinal.push(times[timeSorteado])
     }
-    grupoFinal.push(timeSorteado)
+    
+    console.log(grupoFinal) */
 }
 
-console.log(grupoFinal)
-
-
-
-
-/* fetch('https://estagio.geopostenergy.com/WorldCup/GetAllTeams', {
-    method: 'GET',
-    headers: {
-        'git-user': 'giacomop8',
-    },
-})
-.then((response) => response.json())
-.then((data) => {
-    let listaSelecoes = data.Result
-    let grupos = []
-    let jaSorteados = []
-    let qtdSelecoes = listaSelecoes.length
-    let numero = 0
-    let contNaoExiste = 0
-    let contExiste = 0
-    
-    
-
-
-
-}) */
+let dados = getContent()
