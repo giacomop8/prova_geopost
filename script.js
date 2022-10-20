213f3072502aa95a27a6af5735c7a01710997d65
giacomop8 <giacomopv@outlook.com>
2022-10-20 17:09:26 -0300

    Grupos na tela

--- script.js
+++ script.js
@@ -18,26 +18,36 @@ async function getContent() {
 async function sorteando() {
     
     const times = await getContent()
+    let grupos = []
 
     times.sort(() => Math.random() - 0.5)
     
-    const grupoA = times.slice(0,4)
-    const grupoB = times.slice(4,8)
-    const grupoC = times.slice(8,12)
-    const grupoD = times.slice(12,16)
-    const grupoE = times.slice(16,20)
-    const grupoF = times.slice(20,24)
-    const grupoG = times.slice(24,28)
-    const grupoH = times.slice(28,32)
+    grupos.push(grupoA = times.slice(0,4))
+    grupos.push(grupoB = times.slice(4,8))
+    grupos.push(grupoC = times.slice(8,12))
+    grupos.push(grupoD = times.slice(12,16))
+    grupos.push(grupoE = times.slice(16,20))
+    grupos.push(grupoF = times.slice(20,24))
+    grupos.push(grupoG = times.slice(24,28))
+    grupos.push(grupoH = times.slice(28,32))
 
-    console.log(grupoA)
-    console.log(grupoB)
-    console.log(grupoC)
-    console.log(grupoD)
-    console.log(grupoE)
-    console.log(grupoF)
-    console.log(grupoG)
-    console.log(grupoH)
+    return grupos
 }
 
-sorteando()
+async function mostrarTela() {
+
+    const grupos = await sorteando()
+    const dados = document.getElementById('dados')
+
+    let html = ''
+
+    for(let i=0; i<8; i++) {
+        html += `<h1> Grupo ${i+1} </h1>`
+        for(let j=0; j<4; j++) {
+            html += `<li> ${grupos[i][j].Name} </li>`
+        }        
+    }
+    dados.innerHTML += html
+}
+
+mostrarTela()
